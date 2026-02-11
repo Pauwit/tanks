60aa3472a17070c773f62f2a55c99d6ed9665d19
Paul Witkowski <paul.witkowski@epita.fr>
2026-02-11 21:50:09 +0100

    corrected API mistake

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,25 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {authenticate} from "./auth.ts";
 import {Logger} from "../game/misc/logger.ts";
 
-// T ODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 export class Firebase {
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     private readonly _firebaseConfig = {
-        apiKey: "AIzaSyDi21KsNnjr71Tff8BV9riXcJKt_DHJia4",
-        authDomain: "tanks-bf67b.firebaseapp.com",
-        databaseURL: "https://tanks-bf67b-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "tanks-bf67b",
-        storageBucket: "tanks-bf67b.firebasestorage.app",
-        messagingSenderId: "882607534193",
-        appId: "1:882607534193:web:a0cd040d87e6c469142bd0",
-        measurementId: "G-L79P1VMSPP"
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
     };
 
     public static readonly MIN_NAME_SIZE = 4;
